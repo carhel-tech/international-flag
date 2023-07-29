16261c1001b36dded5fd386347e942dba27f8a11
Alphonse Carhel MELEDJE <alphonse.meledje@gmail.com>
2023-07-29 20:02:09 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+
+
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A PROPOS</h1>
+            <br />
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui optio consequatur soluta et
+                eveniet, provident deleniti quod enim. Cumque maiores nesciunt iure necessitatibus? Veniam,
+                ratione. Aliquid totam ex rerum, officia praesentium optio asperiores illo quasi sequi
+                consectetur ducimus iusto aspernatur aut laboriosam fuga facere inventore quis. Nemo quae
+                ullam tempora corporis rerum aperiam architecto alias soluta aspernatur ut. Unde sapiente eveniet
+                quae minus quod iure ducimus quaerat quidem provident, illum aperiam blanditiis quos soluta cum
+                nesciunt temporibus dicta veniam sed nostrum? Placeat unde, explicabo, animi harum iste
+                saepe maiores libero corrupti nihil commodi quas impedit. Perferendis vitae ipsa suscipit?
+
+            </p>
+            <br />
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis aut sed quod, cumque eligendi officia numquam fugiat deleniti ab eaque atque adipisci, exercitationem possimus quae corrupti. Qui at cum vero!
+            </p>
+        </div>
+    );
+};
+
+export default About;
